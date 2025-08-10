820955e48146b7edffdbc1f31ae5b1e08edf0646
Abdulmajid <mugambbo@gmail.com>
2025-08-10 18:00:18 +0100

    Initial Commit

---
+++ Toomti Tracker/Toomti_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toomti_TrackerApp.swift
+//  Toomti Tracker
+//
+//  Created by Abdulmajid Isiaka on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Toomti_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Toomti TrackerTests/Toomti_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Toomti_TrackerTests.swift
+//  Toomti TrackerTests
+//
+//  Created by Abdulmajid Isiaka on 10/08/2025.
+//
+
+import Testing
+@testable import Toomti_Tracker
+
+struct Toomti_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
